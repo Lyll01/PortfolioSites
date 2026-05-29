d26f919fd03a999436a0e2b88da3b69a542b8433
LylDerr <dert3k@gmail.com>
2026-05-29 15:17:41 +0200

    config vercel

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,11 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-const basePath = isProd ? "/PortfolioSites" : "";
-
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath,
   env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
+    NEXT_PUBLIC_BASE_PATH: "",
   },
   allowedDevOrigins: ["192.168.1.54", "localhost", "172.20.10.2"],
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
